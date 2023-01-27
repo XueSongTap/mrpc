c9fa7b65fc7666e937620bab5918d8363a18e786
yxc <yxc2020@whu.edu.cn>
2023-01-27 13:18:53 +0800

    finish caller of rpc

--- example/caller/calluserservice.cc
+++ example/caller/calluserservice.cc
@@ -1,14 +1,25 @@
 #include <iostream>
 #include "mprpcapplication.h"
 #include "user.pb.h"
-#include ""
+#include "mprpcchannel.h"
+
 int main(int argc, char** argv) {
     MprpcApplication::Init(argc, argv);
-
-    MprpcApplication::Init(argc, argv);
-
     fixbug::UserServiceRpc_Stub stub(new MprpcChannel());
+    
+    //rpc方法请求参数
+    fixbug::LoginRequest request;
+    request.set_name("zhang san");
+    request.set_pwd("123456");
 
-    stub.Login(, const ::fixbug::LoginRequest *request, ::fixbug::LoginResponse *response, ::google::protobuf::Closure *done);
+    fixbug::LoginResponse response;
+    stub.Login(nullptr, &request, &response, nullptr);
+
+    //rpc调用完成，返回结果
+    if (0 == response.result().errcode()){
+        std::cout << "rpc login response success:" << response.success() << std::endl;
+    }else {
+        std::cout << "rpc login response error : " << response.result().errmsg() << std::endl;
+    }
     return 0;
 }
--- src/include/mprpcchannel.h
+++ src/include/mprpcchannel.h
@@ -4,11 +4,13 @@
 #include <google/protobuf/service.h>
 #include <google/protobuf/stubs/callback.h>
 
-class MprpcChannel : public google::protobuf::RpcChannel{
+class MprpcChannel : public google::protobuf::RpcChannel
+{
 public:
-    void callMethod(const google::protobuf::MethodDescriptor* method,
-                    google::protobuf::RpcController* controller, const google::protobuf::Message* request,
-                    google::protobuf::Message* response, google::protobuf::Closure* done);
-
-private:
+    // 所有通过stub代理对象调用的rpc方法，都走到这里了，统一做rpc方法调用的数据数据序列化和网络发送 
+    void CallMethod(const google::protobuf::MethodDescriptor* method,
+                          google::protobuf::RpcController* controller, 
+                          const google::protobuf::Message* request,
+                          google::protobuf::Message* response,
+                          google::protobuf:: Closure* done);
 };
